3db07e9caaf48a2a3dac670cb43ee39ede952ca4
gnchishti <gchishti@gmail.com>
2022-07-16 06:54:51 +0500

    Initial Commit

---
+++ SwiftUITabsApp/SwiftUITabsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITabsAppApp.swift
+//  SwiftUITabsApp
+//
+//  Created by gnc on 16/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITabsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
